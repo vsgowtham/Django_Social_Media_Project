831354ffad70ea1fd8692ccfc8e42e3223e2771c
vsgowtham <vsgowtham18@gmail.com>
2023-05-19 11:43:40 +0530

    done

---
+++ media_app/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+#User
+User=get_user_model()
+
+#Profile
+class Profile(models.Model):
+    user=models.ForeignKey(User,related_name='profile',on_delete=models.CASCADE)
+    decription=models.TextField(blank=True,null=True)
+    image=models.ImageField(blank=True,null=True)
+    location=models.CharField(max_length=100)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table='profile'
+#Post
+class Post(models.Model):
+    user=models.ForeignKey(User,related_name='post',on_delete=models.CASCADE)
+    caption=models.TextField(blank=True,null=True)
+    image=models.ImageField(blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table='post'
+
+#LikePost
+class LikePost(models.Model):
+    user=models.ForeignKey(User,related_name='like_post',on_delete=models.CASCADE)
+    post=models.ForeignKey(Post,related_name='like_post',on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table='like_post'
+
--- social_media_project/urls.py
+++ social_media_project/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from media_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('signup/', views.sign_up, name='sign_up' ),
+    path('signin/', views.sign_in, name='sign_in'),
+    path('signout/', views.sign_out, name='sign_out'),
+    path('profile-settings/', views.profile_settings, name='profile_settings'),
+    path('add_post/', views.add_post, name='add_post'),
+    path('like_post/<int:post_id>/', views.like_post, name='like_post'),
+    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('block_user/<int:user_id>/', views.block_user, name='block_user'),
+    path('make_admin/<int:user_id>/', views.make_admin, name='make_admin')
+
 ]
